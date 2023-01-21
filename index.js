ba995c689c5470eb19f95dd32ca59b1b29149b19
Avi-sa <avivish000@gmail.com>
2023-01-22 00:24:27 +0530

    Added formatter prettier and defined Scrapping Functions
    - Configured Prettier
    - Added Dependency
    - scrapping function

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 // Importing the express module
-const express = require("express");
+const express = require('express');
 
 // calling the express function
 const app = express();
 
-const scraper = require("./web_scrape.js");
+const scraper = require('./web_scrape.js');
 
 // Creating a "/home" route for sending "Hello World!😎😎" to the clientSide(Browser)
-app.get("/home", (req, res) => {
-  res.status(200).send("<h1>Hello Worlds!😎😎</h1>");
+app.get('/home', (req, res) => {
+	res.status(200).send('<h1>Hello Worlds!😎😎</h1>');
 });
 
 // Route for xiaomi_redmi_note_10_pro-10662.php
-app.get("/api", async (req, res) => {
-  try {
-    console.log("2 .Inside");
-    const model_prices = await scraper.getAllSearchResults(
-      "Redmi note 10",
-      "https://www.gsmarena.com/"
-    );
-    console.log(model_prices);
-    return res.status(200).json({
-      result: model_prices,
-    });
-  } catch (err) {
-    return res.status(500).json({ err: err.toString() });
-  }
+app.get('/api', async (req, res) => {
+	try {
+		console.log('2 .Inside');
+		const model_prices = await scraper.getAllSearchResults(
+			'Redmi note 10',
+			'https://www.gsmarena.com/',
+		);
+		console.log(model_prices);
+		return res.status(200).json({
+			result: model_prices,
+		});
+	} catch (err) {
+		console.log(err.toString());
+		return res.status(500).json({ err: err.toString() });
+	}
 });
 
 // declaring our Port number variable
@@ -33,5 +34,5 @@ const PORT = 4000 || process.env.PORT;
 
 // Creating a server with the PORT variable declared above
 app.listen(PORT, () => {
-  console.log(`Listening to Port ${PORT}`);
+	console.log(`Listening to Port ${PORT}`);
 });
--- web_scrape.js
+++ web_scrape.js
@@ -2,126 +2,92 @@
 Axios is used to make HTTP requests. Run the command below to install the 
 dependency.
 */
-const axios = require("axios");
+const axios = require('axios');
 
 /*
 Cheerio helps to parse markup, it is used to pick out HTML elements from 
 a webpage.
 */
-const cheerio = require("cheerio");
+const cheerio = require('cheerio');
 
 /* 
     Function returns the list of Prices of the particular model mobile url.
     It also returns the purchase url for each possible price. 
 */
 const dataScraper = async function getDataScraper(url) {
-  const priceArray = [];
-  await axios(url).then((response) => {
-    const html_data = response.data;
-    const $ = cheerio.load(html_data);
+	const priceArray = [];
+	await axios(url).then((response) => {
+		const html_data = response.data;
+		const $ = cheerio.load(html_data);
 
-    const selectedElem =
-      "#wrapper > #outer > #body > div.main.main-review.right.l-box.col > table > tbody > tr";
+		const selectedElem =
+			'#wrapper > #outer > #body > div.main.main-review.right.l-box.col > table > tbody > tr';
 
-    $(selectedElem).each((parentIndex, parentElem) => {
-      const priceDetails = {};
-      if (parentIndex >= 0) {
-        $(parentElem)
-          .children()
-          .each((_, childElem) => {
-            var value = $(childElem).text();
-            var urls = $(childElem).find("a").attr("href");
-            if (value && urls) {
-              priceDetails["price"] = value;
-              priceDetails["purchase_url"] = urls;
-            }
-          });
-        priceArray.push(priceDetails);
-      }
-    });
-  });
-  return priceArray;
+		$(selectedElem).each((parentIndex, parentElem) => {
+			const priceDetails = {};
+			if (parentIndex >= 0) {
+				$(parentElem)
+					.children()
+					.each((_, childElem) => {
+						var value = $(childElem).text();
+						var urls = $(childElem)
+							.find('a')
+							.attr('href');
+						if (value && urls) {
+							priceDetails['price'] = value;
+							priceDetails['purchase_url'] = urls;
+						}
+					});
+				priceArray.push(priceDetails);
+			}
+		});
+	});
+	return priceArray;
 };
 
 const getAllSearchResults = async (search, url) => {
-  const searchKey = search.replaceAll(" ", "+");
-  const searchUrl = url + "res.php3?sSearch=" + searchKey;
+	const searchKey = search.replaceAll(' ', '+');
+	const searchUrl = url + 'res.php3?sSearch=' + searchKey;
 
-  console.log(searchUrl);
+	const response = await axios(searchUrl);
+	const html_data = response.data;
+	const $ = cheerio.load(html_data);
 
-  const res = {};
+	const selectedElem =
+		'#wrapper > #outer > #body > \
+    div.main.right.main-maker.l-box.col.search-results > #review-body > \
+    div.makers > ul';
 
-  await axios(searchUrl).then((response) => {
-    const html_data = response.data;
-    const $ = cheerio.load(html_data);
+	const result = {};
 
-    const selectedElem =
-      "#wrapper > #outer > #body > \
-        div.main.right.main-maker.l-box.col.search-results > #review-body > \
-        div.makers > ul";
-
-    $(selectedElem).each(async (parentIndex, parentElem) => {
-      const results = [];
-      const model_names = [];
-
-      if (parentIndex >= 0) {
-        $(parentElem)
-          .children()
-          .each((_, childElem) => {
-            var value = $(childElem).text();
-            var mobile_url = $(childElem).find("a").attr("href");
-            mobile_url = url + mobile_url;
-
-            // console.log(mobile_url);
-
-            const model_wise_data = dataScraper(mobile_url);
-            // let t;
-            // const device_data = async () =>{
-            //     return await dataScraper(mobile_url);
-            // };
-
-            // const device_data = dataScraper(mobile_url);
-
-            // console.log(device_data);
-
-            // let device_price = "";
-            // device_data().then((res)=>{
-            //         // console.log(JSON.stringify(res), typeof(JSON.stringify(res)));
-            //         // device_price = res;
-            //         console.log(res);
-            //         // results["mobile"] = value;
-            //         // results["more_info"] = JSON.stringify(res);
-            //         // return res;
-            //     })
-
-            // device_data();
-            // console.log(t);
-            // console.log(device);
-            // console.log("device_price === ", device_price)
-
-            // console.log(results);
-
-            // console.log("model_wise_data = ", model_wise_data);
-
-            results.push(model_wise_data);
-            model_names.push(value);
-          });
-      }
-
-      const all_models_details = await Promise.all(results);
-      console.log("All models details === ", all_models_details);
-      // console.log("All models == ", model_names);
-      model_names.forEach((element, i) => {
-        res[element] = all_models_details[i];
-      });
-      console.log("res == ", res);
-    });
-  });
-
-  return res;
+	var response_data = $(selectedElem).map(async (parentIndex, parentElem) => {
+		const results = [];
+		const model_names = [];
+		if (parentIndex >= 0) {
+			$(parentElem)
+				.children()
+				.each((_, childElem) => {
+					var value = $(childElem).text();
+					var mobile_url = $(childElem)
+						.find('a')
+						.attr('href');
+					mobile_url = url + mobile_url;
+					const model_wise_data = dataScraper(mobile_url);
+					results.push(model_wise_data);
+					model_names.push(value);
+				});
+		}
+		const all_models_details = await Promise.all(results);
+		model_names.forEach((element, i) => {
+			result[element] = all_models_details[i];
+		});
+		return result;
+	});
+	await Promise.all(response_data);
+	return result;
 };
 
 module.exports = {
-  dataScraper,
-  getAllSearchResults,
+	dataScraper,
+	getAllSearchResults,
 };
