93d0f59fb035a47be05912ea54ddec664905d912
Avi-sa <avivish000@gmail.com>
2023-01-21 23:33:06 +0530

    Scraped Data using node server

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 // Importing the express module
-const express = require('express'); 
+const express = require("express");
+
 // calling the express function
-const app = express(); 
+const app = express();
+
+const scraper = require("./web_scrape.js");
 
 // Creating a "/home" route for sending "Hello World!😎😎" to the clientSide(Browser)
-app.get("/home", (req, res)=>{
-    res.status(200).send("<h1>Hello World!😎😎</h1>")
-})
+app.get("/home", (req, res) => {
+  res.status(200).send("<h1>Hello Worlds!😎😎</h1>");
+});
+
+// Route for xiaomi_redmi_note_10_pro-10662.php
+app.get("/api", async (req, res) => {
+  try {
+    console.log("2 .Inside");
+    const model_prices = await scraper.getAllSearchResults(
+      "Redmi note 10",
+      "https://www.gsmarena.com/"
+    );
+    console.log(model_prices);
+    return res.status(200).json({
+      result: model_prices,
+    });
+  } catch (err) {
+    return res.status(500).json({ err: err.toString() });
+  }
+});
 
 // declaring our Port number variable
 const PORT = 4000 || process.env.PORT;
 
-console.log(PORT)
-
 // Creating a server with the PORT variable declared above
-app.listen(PORT, ()=>{
-    console.log(`Listening to Port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Listening to Port ${PORT}`);
 });
-
